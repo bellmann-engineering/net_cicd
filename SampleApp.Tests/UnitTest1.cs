527e7d2838aa88909bd4a25f1cf84e17016e847f
Kai Bellmann <kb@bellmann-engineering.com>
2023-06-11 18:39:31 +0200

    added tests

---
+++ SampleApp.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace SampleApp.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+		Assert.IsTrue(true);
+    }
+}
